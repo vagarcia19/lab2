495052eeec5e7888ba3491de00f8ef68388e1199
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-02-08 20:13:43 +0800

    PHP Script (Variables)

---
+++ week8/index2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+
+echo $txt;
+echo "<br>";
+echo "<br>";
+echo $x;
+echo "<br>";
+echo "<br>";
+echo $y;
+?>
+
+</body>
+</html>
