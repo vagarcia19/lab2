2eb0ce42cea64a36e80bd26511560fbbfeea3e28
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-02-08 21:04:11 +0800

    Week 9 PHP Form

--- week9/Reg.php
+++ week9/Reg.php
@@ -1,6 +1,20 @@
 <!DOCTYPE HTML>  
 <html>
 <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css">
+    
+    <!-- Custom Css -->
+    
+    <title>Vieron's Personal Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 <style>
 .error {color: #FF0000;}
 </style>
@@ -99,6 +113,14 @@ echo $comment;
 echo "<br>";
 echo $gender;
 ?>
-
+<header>
+    <a href="#" class="logo">vieron</a>
+    <ul>
+        <li><a href="index.php" class="active">Home</a></li>
+        <li><a href="https://www.facebook.com/dakto.box">Facebook</a></1i>
+        <li><a href="Reg.php">Register</a></1i>
+    </ul>
+</header>
+    <img src="stars.png" id="stars" width="120%" height="120%">
 </body>
 </html>
