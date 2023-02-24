3121669fea91a3c601f33fbac37f16a180d662b7
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-02-24 15:29:06 +0800

    apcwebprog table (week10)

--- week9/sql_connect.php
+++ week9/sql_connect.php
@@ -3,6 +3,8 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 
+
+
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 
--- week9/vagarcia_guests.php
+++ week9/vagarcia_guests.php
@@ -11,7 +11,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 $sql = "SELECT id, name, email, website, comment, gender FROM MyGuests";
 $result = $conn->query($sql);
 
