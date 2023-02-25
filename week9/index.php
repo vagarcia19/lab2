5c3a94a871cefcb00d4dcfc5549f8144621fb198
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-02-25 15:42:47 +0800

    update

--- week9/index.php
+++ week9/index.php
@@ -37,6 +37,30 @@
 </section>
 
 <div id="sec">
+  <div class="col-sm-4 col-md-3 item">
+    <h3>Services</h3>
+    <ul>
+        <li><a href="#">Web design</a></li>
+        <li><a href="#">Development</a></li>
+        <li><a href="#">Hosting</a></li>
+    </ul>
+  </div>
+  <div class="col-sm-4 col-md-3 item">
+    <h3>About</h3>
+    <ul>
+        <li><a href="#">Company</a></li>
+        <li><a href="#">Team</a></li>
+        <li><a href="#">Legacy</a></li>
+    </ul>
+  </div>
+  <div class="col-sm-4 col-md-3 item">
+    <h3>Careers</h3>
+    <ul>
+        <li><a href="#">Job openings</a></li>
+        <li><a href="#">Employee success</a></li>
+        <li><a href="#">Benefits</a></li>
+    </ul>
+  </div>
   
 </div>
 <script>
