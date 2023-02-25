b44dfdb3a5de9aaae05441509c6f4c22f7a75d12
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-02-25 14:52:08 +0800

    update

--- week9/vagarcia_guests.php
+++ week9/vagarcia_guests.php
@@ -11,7 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT id, name, email, website, comment, gender FROM vagarcia_guests";
+$sql = "SELECT id, name, email, website, comment, gender FROM vagarcia_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
